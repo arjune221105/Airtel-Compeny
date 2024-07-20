632b5511ac8273c8efddc3afa436c60442b898a3
arjune221105 <shakti221105@gmail.com>
2024-07-19 20:02:17 -0700

    push

---
+++ Demo-AirtelandVIwork/src/test/java/Employee/ToCreateEmployee.java
@@ -0,0 +1,10 @@
+package Employee;
+
+public class ToCreateEmployee {
+
+	public static void main(String[] args) {
+		System.out.println("TO Create Employee Person From Airtel Compeny");
+		System.out.println("Total Employee 1K");
+	}
+
+}
